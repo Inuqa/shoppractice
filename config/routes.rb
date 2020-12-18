c394549468d9b780fd9c5ba6da37319617809749
Sebastian Rojas <seba.nrc@gmail.com>
2020-12-18 08:23:09 -0300

    added haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/remove'
+  resources :products, only: %i[index show new create remove]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
