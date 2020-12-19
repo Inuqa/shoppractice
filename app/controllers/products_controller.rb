3ca1f05c83b3e6f85b52490e1e852a728cecdd51
Sebastian Rojas <seba.nrc@gmail.com>
2020-12-19 09:28:02 -0300

    added Category model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,15 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find_by(params)
   end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
-  def remove
-  end
+  def remove; end
 end
