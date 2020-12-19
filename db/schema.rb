8f45d264469cd604c7860348f3b128f5c7f6122e
Sebastian Rojas <seba.nrc@gmail.com>
2020-12-19 09:51:48 -0300

    Added category model and created category_id column in products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_110252) do
+ActiveRecord::Schema.define(version: 2020_12_19_124305) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_12_18_110252) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
