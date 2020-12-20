24d42acfef0805460612ab7fc02884c5593d0598
Sebastian Rojas <seba.nrc@gmail.com>
2020-12-20 04:01:06 -0300

    added warden and created sessions controller

--- Gemfile
+++ Gemfile
@@ -22,7 +22,10 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
-gem 'rails_warden'
+git 'https://github.com/wardencommunity/rails_warden' do
+  gem 'rails_warden'
+end
+
 # Use Active Storage variant
 gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  resources :products, only: %i[index show new create destroy]
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
