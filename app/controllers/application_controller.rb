6af81e6f7305ef015feb9613a727ac598f71f659
Sebastian Rojas <seba.nrc@gmail.com>
2020-12-21 13:33:19 -0300

    added login to application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include RailsWarden::Authentication
+
+  def current_user
+    warden.user
+  end
+
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   resources :sessions, only: %i[new create destroy]
+  get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 end
